69bc9540781803d9ff2de091950b673ac0186109
Felipe <cvfelipeoliveira@gmail.com>
2020-07-17 15:28:39 -0400

    first

---
+++ src/main/java/br/com/jenkins/Main.java
@@ -0,0 +1,15 @@
+package br.com.jenkins;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+import org.slf4j.Logger;
+
+public class Main {
+	
+	private static final Logger LOG = getLogger(Main.class);
+	
+	public static void main(String[] args) {
+		LOG.info("yess!!!!");
+	}
+
+}
